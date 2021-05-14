c157296d29149098fd0b1a82c8e31ab1f084f2df
proPrateekSahu <prateeksahu2706@gmail.com>
2021-05-14 16:42:11 +0530

    initial commit

---
+++ flutter_app_practise/android/app/src/main/kotlin/com/example/flutterapppractise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterapppractise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app_practise/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+        ),
+        body: Container(
+          color: Colors.teal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  )
+                ],
+              )),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
